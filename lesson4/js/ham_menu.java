6c3a5ca8c1837b685d52c906fe81a2fd59a65838
mcallisterpeter <pmcalli@byui.edu>
2020-10-09 18:50:41 -0600

    trying to fix java stuff

--- lesson4/js/ham_menu.java
+++ lesson4/js/ham_menu.java
@@ -1,12 +1,3 @@
-// Display current time
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
-
-//Collapsable navigation
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
@@ -20,4 +11,5 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
   see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
   or https://www.w3schools.com/js/js_arrow_function.asp
 
-  classList property - https://developer.mozilla.org/en-US/docs/Web/
+  classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  */
