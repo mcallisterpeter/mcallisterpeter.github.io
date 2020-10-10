c4ab561e81c3805e926b3f27d2dfe0b6c500db1d
mcallisterpeter <pmcalli@byui.edu>
2020-10-09 18:18:43 -0600

    Updated CSS for responsive navigation

--- lesson4/js/myjava.java
+++ lesson4/js/myjava.java
@@ -7,6 +7,17 @@ try {
 }
 
 //Collapsable navigation
-function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*** Programming Notes **************************************
+  Arrow Functions - es6 syntactically compact alternative to a regular function expression
+  see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+  or https://www.w3schools.com/js/js_arrow_function.asp
+
+  classList property - https://developer.mozilla.org/en-US/docs/Web/
