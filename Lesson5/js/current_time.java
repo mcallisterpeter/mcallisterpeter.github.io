28db87f49677a9a135761982daaefe58e28e9251
mcallisterpeter <pmcalli@byui.edu>
2020-10-14 18:32:03 -0600

    removed lesson 5

--- Lesson5/js/current_time.java
+++
@@ -1,7 +0,0 @@
-// Display current time
-try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
-}
