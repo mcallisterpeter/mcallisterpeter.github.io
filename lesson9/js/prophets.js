c4a534b51595af8a67834707e2ac3704445c623e
mcallisterpeter <pmcalli@byui.edu>
2020-11-11 17:15:02 -0700

    added the for loop back in

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -5,5 +5,16 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject);
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+
+        card.appendChild(h2);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+    const prophets = jsonObject['prophets'];
   });
