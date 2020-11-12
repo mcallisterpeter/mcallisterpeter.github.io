fa8812222a836ec0ed3b7c636f8113724058adb6
mcallisterpeter <pmcalli@byui.edu>
2020-11-11 17:02:39 -0700

    tried just copying and pasting to see if I missed typed something

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,22 +1,10 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        console.table(jsonObject);
-        for (let i = 0; i < prophets.length; i++) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject    console.table(jsonObject);  // temporary checking for valid response and data parsing
+  });
 
-            h2.textContent = propjets[i].name + ' ' + prophets[i].lastname;
-
-            card.appendChild(h2);
-
-            document.querySelector('div.cards').appendChild(card);
-        }
-    });
-
-const prophets = jsonObject['prophets'];
-
+  const prophets = jsonObject['prophets'];
