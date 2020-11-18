918a1050dad8a2e118595220ad675b5e28227928
mcallisterpeter <pmcalli@byui.edu>
2020-11-17 18:13:45 -0700

    updated js to https reference

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=3efcd4225e37daee266d0b251f791a4b&units=imperial';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=3efcd4225e37daee266d0b251f791a4b&units=imperial';
 
 fetch(apiURL)
   .then((response) => response.json())
