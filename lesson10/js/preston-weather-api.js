8f0b9b4ab67f2ebe248207d798d45b658f803d32
mcallisterpeter <pmcalli@byui.edu>
2020-11-17 19:26:38 -0700

    Weather summary replaced with api data

--- lesson10/js/preston-weather-api.js
+++ lesson10/js/preston-weather-api.js
@@ -4,12 +4,8 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
-
+    document.getElementById('tempF').textContent = jsObject.main.temp;
+    document.getElementById('humid').textContent = jsObject.main.humidity;
+    document.getElementById('speed').textContent = jsObject.wind.speed;
+    document.getElementById('currently').textContent = jsObject.weather[0].main;
   });
