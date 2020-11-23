cda07bd7b8ce3738cf280cb31652de71649276ee
mcallisterpeter <pmcalli@byui.edu>
2020-11-23 16:52:07 -0700

    fixed link path in thanks.js

--- lesson11/js/thanks.js
+++ lesson11/js/thanks.js
@@ -1,3 +1,3 @@
 function navThanks() {
-    window.location.href="https://mcallisterpeter.github.io/lesson10/thanks.html";
+    window.location.href="https://mcallisterpeter.github.io/lesson11/thanks.html";
 }
