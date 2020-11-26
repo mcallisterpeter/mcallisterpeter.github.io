bc3e19f5ce0f00d0549ec3c72b60f0466e7f89fb
mcallisterpeter <pmcalli@byui.edu>
2020-11-25 20:34:45 -0700

    added town events

--- lesson11/js/townjson.js
+++ lesson11/js/townjson.js
@@ -1,3 +1,5 @@
+let myPath = window.location.pathname;
+
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
@@ -8,43 +10,62 @@ fetch(requestURL)
     console.table(jsonObject);
     const towns = jsonObject['towns'];
     for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') {
-            let card = document.createElement('section');
-            let towninfo = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let motto = document.createElement('p');
-            let found = document.createElement('p');
-            let pop = document.createElement('p');
-            let rain = document.createElement('p');
-            let image = document.createElement('img');
-            
-            card.appendChild(towninfo);
+      if (towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') {
+        if (myPath.includes("index.html")) {
+          let card = document.createElement('section');
+          let towninfo = document.createElement('section');
+          let h2 = document.createElement('h2');
+          let motto = document.createElement('p');
+          let found = document.createElement('p');
+          let pop = document.createElement('p');
+          let rain = document.createElement('p');
+          let image = document.createElement('img');
+          
+          card.appendChild(towninfo);
 
-            card.setAttribute('class', 'townsect');
+          card.setAttribute('class', 'townsect');
 
-            towninfo.setAttribute('class', 'towninfo');
+          towninfo.setAttribute('class', 'towninfo');
 
-            h2.textContent = towns[i].name;
-            towninfo.appendChild(h2);
+          h2.textContent = towns[i].name;
+          towninfo.appendChild(h2);
 
-            motto.textContent = towns[i].motto;
-            towninfo.appendChild(motto);
+          motto.textContent = towns[i].motto;
+          towninfo.appendChild(motto);
 
-            found.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            towninfo.appendChild(found);
+          found.textContent = 'Year Founded: ' + towns[i].yearFounded;
+          towninfo.appendChild(found);
 
-            pop.textContent = 'Population: ' + towns[i].currentPopulation;
-            towninfo.appendChild(pop);
+          pop.textContent = 'Population: ' + towns[i].currentPopulation;
+          towninfo.appendChild(pop);
 
-            rain.textContent = 'Annual Rain Fall: ' + towns[i].currentPopulation;
-            towninfo.appendChild(rain);
-            
-            image.setAttribute('class', 'townimg');
-            image.setAttribute('src', 'images/' + towns[i].photo);
-            image.setAttribute('alt', 'Image of ' + towns[i].name);
-            card.appendChild(image);
+          rain.textContent = 'Annual Rain Fall: ' + towns[i].currentPopulation;
+          towninfo.appendChild(rain);
+          
+          image.setAttribute('class', 'townimg');
+          image.setAttribute('src', 'images/' + towns[i].photo);
+          image.setAttribute('alt', 'Image of ' + towns[i].name);
+          card.appendChild(image);
 
-            document.querySelector('div.cards').appendChild(card);
+          document.querySelector('div.cards').appendChild(card);
         }
+        for (let t = 0; t < towns[i].events.length; t++) {
+          if (myPath.includes("preston.html") && towns[i].name == 'Preston') {
+            let event = document.createElement('p');
+            event.textContent = towns[i].events[t];
+            document.querySelector('div.events').appendChild(event);
+          }
+          if (myPath.includes("fish-haven.html") && towns[i].name == 'Fish Haven') {
+            let event = document.createElement('p');
+            event.textContent = towns[i].events[t];
+            document.querySelector('div.events').appendChild(event);
+          }
+          if (myPath.includes("soda-springs.html") && towns[i].name == 'Soda Springs') {
+            let event = document.createElement('p');
+            event.textContent = towns[i].events[t];
+            document.querySelector('div.events').appendChild(event);
+          }
+        }
+      }
     }
   });
