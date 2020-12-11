88e02761908a51712b2860a61a4fba150704a47b
mcallisterpeter <pmcalli@byui.edu>
2020-12-11 15:16:03 -0700

    finished rental page

--- final/js/rental_api.js
+++ final/js/rental_api.js
@@ -9,7 +9,9 @@ fetch(forApi)
     const rental = jsObject['rental'];
     for (let i = 0; i < rental.length; i++ ) {
         let card = document.createElement('section');
+        let img_card = document.createElement('section');
         let h1 = document.createElement('h1');
+        let max_h2 = document.createElement('h2');
         let max = document.createElement('p');
         let res_h2 = document.createElement('h2');
         let res = document.createElement('p');
@@ -17,10 +19,15 @@ fetch(forApi)
         let wlk = document.createElement('p');
         let image = document.createElement('img');
 
+        card.setAttribute('class', 'cards');
+
         h1.textContent = rental[i].vehicle;
         card.appendChild(h1);
 
-        max.textContent = 'Maximum People: ' + rental[i].max_p;
+        max_h2.textContent = 'Maximum Passengers';
+        card.appendChild(max_h2);
+
+        max.textContent = rental[i].max_p;
         card.appendChild(max);
 
         res_h2.textContent = 'Price for Reservations';
@@ -34,10 +41,13 @@ fetch(forApi)
 
         wlk.textContent = 'Half Day Rental: $' + rental[i].walk_in.hlf_day + '.00 | Full Day Rental: $' + rental[i].walk_in.fll_day + '.00';
         card.appendChild(wlk);
+
+        card.appendChild(img_card);
+        img_card.setAttribute('class', 'rent_img');
         
         image.setAttribute('src', rental[i].img_src);
         image.setAttribute('alt', rental[i].img_alt);
-        card.appendChild(image);
+        img_card.appendChild(image);
 
         document.querySelector('div.rent_cards').appendChild(card);
     }
